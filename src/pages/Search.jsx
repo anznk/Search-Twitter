90a32ccca72aae51f35f9d8cbccb73b69b521903
anznk <anzunakayama@gmail.com>
2021-12-06 14:02:44 -0800

    chear input value after submitting

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,18 +2,21 @@ import React, { useState} from "react";
 import axios from 'axios';
 
 const Search = () => {
-  const [search, setSearch] = useState("Vancouver");
+  const [search, setSearch] = useState("");
+  
   const handleChange = event => {
     setSearch(event.target.value);
   }
   const handleSubmit = event => {
-    console.log("seach word", search);
     event.preventDefault();
     axios.post(`http://localhost:3000/register`, { "searchtag": search},{mode: 'cors'})
       .then(res => {
         console.log("res", res);
         console.log("res.data", res.data);
       })
+
+    // clear input box value
+    setSearch("");
 
   }
   
@@ -25,9 +28,10 @@ const Search = () => {
           type="text"
           className="search_container"
           id="search"
-          placeholder="Hash Tag"
+          placeholder="search word"
           name="search"
           autocomplete="off"
+          value={search}
         />
         <button type="submit" className="button">
           Submit
