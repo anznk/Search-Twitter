ea2d9bd9884eaf7cae27b3b29f706bd45ab1db2a
sander <demeester.sander@gmail.com>
2021-12-05 21:38:23 -0800

    react app update

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -5,6 +5,7 @@ const Result = () => {
     const [tweets, setTweets ] = useState([]);
     const [searchTag, setSearchTag ] = useState();
     
+    let flag = false;
     async function fetchData() {
       // get Search word
       const res = await axios.get(`http://localhost:3000/results`, {mode: 'cors'});
@@ -14,17 +15,27 @@ const Result = () => {
         const value = items.sort(function (a, b) {
           return b.date - a.date;
         });
+
         setSearchTag(value[0].searchtag);
-        console.log("value[0].searchtag", value[0].searchtag);
+        // console.log(value[0].searchtag);
+        // console.log("value[0].searchtag", searchTag);
       }
-      if(searchTag){
-        fetchTweets(searchTag);
-      }
+      // if(searchTag != "" && searchTag != null){
+      //   fetchTweets(searchTag);
+      // }
     }
+
     async function fetchTweets(keyword) {
       console.log("keyword", keyword);
       // get tweet with search word
-      const tweets = await axios.get('http://localhost:3000/searchtweets?searchtag='+'${keyword}');
+      const tweets = await axios.get('http://localhost:3000/searchtweets?searchtag='+keyword, {
+        // query URL without using browser cache
+        headers: {
+          'Cache-Control': 'no-cache',
+          'Pragma': 'no-cache',
+          'Expires': '0',
+        },
+      });
       const data = tweets.data;
       console.log("data", data);
       if(data){
@@ -33,12 +44,36 @@ const Result = () => {
     }
 
   useEffect(() => {
-    if (tweets.length) {
+    // if (tweets.length > 0) {
+    //   return;
+    // }
+    // fetchData();
+    // if(searchTag !== undefined){
+    //   fetchTweets(searchTag);
+    // }
+    if(flag == false){
+      // first time
+      fetchData();
+      if(searchTag !== undefined){
+        fetchTweets(searchTag);
+      }
+      flag = true;
       return;
     }
-    fetchData();
-  }, [tweets]);
+
+    const interval = setInterval(() => {
+      console.log("timer");
+      fetchData();
+      if(searchTag !== undefined){
+        fetchTweets(searchTag);
+      }
+    }, 10000);
+    return () => clearInterval(interval)
+  }, [tweets, searchTag]);
+
   return (
+    <div>
+      <h1>Result for searchtag: {searchTag}</h1>
     <ol>{
       tweets.map((tweet, i) => {
         return (
@@ -46,8 +81,8 @@ const Result = () => {
         )
       })
     }</ol>
+    </div>
   )
 }
 
 export default Result
-
