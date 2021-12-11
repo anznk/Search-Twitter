2d477516337e27271f9840a041f784fe2987b49a
anznk <anzunakayama@gmail.com>
2021-12-10 18:38:06 -0800

    add tweet feed

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import "../assets/styles/result.scss"
+import CurrentTweet from "../components/CurrentTweet";
 
 const Result = () => {
     const [tweets, setTweets ] = useState([]);
     const [searchTag, setSearchTag ] = useState();
+    const [selectedTweets, setSelectedTweets ] = useState([]);
     
     let flag = false;
     async function fetchData() {
@@ -15,18 +18,12 @@ const Result = () => {
         const value = items.sort(function (a, b) {
           return b.date - a.date;
         });
-
         setSearchTag(value[0].searchtag);
-        // console.log(value[0].searchtag);
-        // console.log("value[0].searchtag", searchTag);
       }
-      // if(searchTag != "" && searchTag != null){
-      //   fetchTweets(searchTag);
-      // }
     }
 
     async function fetchTweets(keyword) {
-      console.log("keyword", keyword);
+      // console.log("keyword", keyword);
       // get tweet with search word
       const tweets = await axios.get('http://localhost:3000/searchtweets?searchtag='+keyword, {
         // query URL without using browser cache
@@ -37,51 +34,88 @@ const Result = () => {
         },
       });
       const data = tweets.data;
-      console.log("data", data);
+      // console.log("data", data);
       if(data){
         setTweets(data);
       }
     }
 
   useEffect(() => {
-    // if (tweets.length > 0) {
-    //   return;
-    // }
-    // fetchData();
-    // if(searchTag !== undefined){
-    //   fetchTweets(searchTag);
-    // }
-    if(flag == false){
-      // first time
-      fetchData();
-      if(searchTag !== undefined){
-        fetchTweets(searchTag);
-      }
-      flag = true;
+    // call fetchTweerts once
+    if (tweets.length) {
       return;
     }
+    fetchTweets("Vancouver");
+    // if(flag === false){
+      // first time
+      // fetchData();
+      // if(searchTag){
+        // fetchTweets(searchTag);
+      // }
+    //   flag = true;
+    //   return;
+    // }
 
-    const interval = setInterval(() => {
-      console.log("timer");
-      fetchData();
-      if(searchTag !== undefined){
-        fetchTweets(searchTag);
-      }
-    }, 10000);
-    return () => clearInterval(interval)
-  }, [tweets, searchTag]);
+    // const interval = setInterval(() => {
+    //   console.log("timer");
+    //   fetchData();
+    //   if(searchTag){
+    //     fetchTweets(searchTag);
+    //   }
+    // }, 10000);
+    // return () => clearInterval(interval)
+  // }, [tweets, searchTag]);
+  }, [tweets]);
 
+  const addTweets = event => {
+    // console.log("event", event);
+
+    setSelectedTweets([...selectedTweets, tweets[event]]);   
+    tweets.splice(event, 1);
+    // setTweets(...tweets, tweets);
+    // console.log("tweets", tweets);
+    
+  }
+  const deleteTweets = event => {
+    selectedTweets.splice(event, 1);  
+    setSelectedTweets([...selectedTweets, selectedTweets]);
+    // console.log("selectedTweets", selectedTweets);
+  }
   return (
-    <div>
-      <h1>Result for searchtag: {searchTag}</h1>
-    <ol>{
+    <div className="main">
+      <h1># 　{searchTag}</h1>
+    <ol className="feed">{
+      tweets && (
       tweets.map((tweet, i) => {
         return (
-          <li key={ i }>{ tweet.text }</li>
+          <li key={ i } className="tweet" onClick={() => addTweets(i)}>
+            <p className="tweet_name">@{tweet.user_name}</p>
+            <p className="tweet_text">{ tweet.text }</p>
+            <p className="tweet_created_at">{tweet.created_at}</p>
+          </li>
         )
       })
+      )
+      }</ol>
+
+    {/* selected tweets */}
+    <ol className="feed">{
+      selectedTweets && (
+      selectedTweets.map((selectedTweet, i) => {
+        return (
+          <li key={ i } className="tweet" onClick={() => deleteTweets(i)}>
+            <p className="tweet_name">{selectedTweet.user_name}</p>
+            <p className="tweet_text">{ selectedTweet.text }</p>
+            <p className="tweet_created_at">{selectedTweet.created_at}</p>
+          </li>
+        )
+      })
+      )
     }</ol>
+    <CurrentTweet selected={selectedTweets} setSelectedTweets={setSelectedTweets}/>
     </div>
+    
+    
   )
 }
 
